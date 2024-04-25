d62eeb2f1c35e3607e2eccc6bcc13c636c380cbe
MannantaB <mannanta18@gmail.com>
2024-04-25 15:11:54 +0700

    memperbaiki menambahkan opsi piliihan

--- Activity5/Activity5.cpp
+++ Activity5/Activity5.cpp
@@ -131,5 +131,18 @@ void searchData() {
 int main() {
     int pilihan;
     do
+    {
+        try
+        {
+            cout << "1. Tambah Data" << endl;
+            cout << "2. Hapus Data" << endl;
+            cout << "3. Tampilkan Data" << endl;
+            cout << "4. Cari Data" << endl;
+            cout << "15. Keluar" << endl;
+            cout << "Pilihan: ";
+            cin >> pilihan;
+            
+        }
+    }
 }
 
